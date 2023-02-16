74ba28c880cffd4b5c81d978f4f4a6c9c470e929
hay12k <hayaangroup93@gmail.com>
2023-02-16 00:57:54 -0800

    new file:   assets/img/b16.jpg 	new file:   assets/img/f2.png 	new file:   assets/img/f3.png 	new file:   assets/img/f4.png 	new file:   assets/img/f5.png 	new file:   assets/img/f6.png 	new file:   assets/img/f7.png 	modified:   assets/script.js 	modified:   assets/style.css 	modified:   index.html

--- assets/script.js
+++ assets/script.js
@@ -4,6 +4,8 @@ document.querySelector('#search-btn').onclick = () =>{
     searchForm.classList.toggle('active');
     shoppingCart.classList.remove('active');
     loginform.classList.remove('active');
+    signupform.classList.remove('active');
+    forgetform.classList.remove('active');
     navbar.classList.remove('active');
 }
 
@@ -13,6 +15,8 @@ document.querySelector('#cart-btn').onclick = () =>{
     shoppingCart.classList.toggle('active');
     searchForm.classList.remove('active');
     loginform.classList.remove('active');
+    forgetform.classList.remove('active');
+    signupform.classList.remove('active');
     navbar.classList.remove('active');
 }
 
@@ -20,6 +24,31 @@ let loginform = document.querySelector('.login-form');
 
 document.querySelector('#login-btn').onclick = () =>{
     loginform.classList.toggle('active');
+    signupform.classList.remove('active');
+    forgetform.classList.remove('active');
+    searchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+
+let signupform = document.querySelector('.signup-form');
+
+document.querySelector('#signup-btn').onclick = () =>{
+    signupform.classList.toggle('active');
+    loginform.classList.remove('active');
+    forgetform.classList.remove('active');
+    searchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+let forgetform = document.querySelector('.forget-form');
+
+document.querySelector('#forget-btn').onclick = () =>{
+    forgetform.classList.toggle('active');
+    loginform.classList.remove('active');
+    signupform.classList.remove('active');
     searchForm.classList.remove('active');
     shoppingCart.classList.remove('active');
     navbar.classList.remove('active');
@@ -29,14 +58,18 @@ let navbar = document.querySelector('.navbar');
 
 document.querySelector('#manu-btn').onclick = () =>{
     navbar.classList.toggle('active');
+    signupform.classList.remove('active');
     searchForm.classList.remove('active');
     shoppingCart.classList.remove('active');
     loginform.classList.remove('active');
+    forgetform.classList.remove('active');
 }
 
 window.onscroll = () =>{
     searchForm.classList.remove('active');
+    signupform.classList.remove('active');
     shoppingCart.classList.remove('active');
     loginform.classList.remove('active');
+    forgetform.classList.remove('active');
     navbar.classList.remove('active');
 }
