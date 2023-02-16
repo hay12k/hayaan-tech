7f5dc99ef84279016f488619b958f1f91e3f032c
hay12k <hayaangroup93@gmail.com>
2023-02-16 06:24:00 -0800

    renamed:    assets/img/WhatsApp Image 2023-02-08 at 01.07.10.jpeg -> assets/img/pro.jpeg 	modified:   assets/script.js 	modified:   assets/style.css 	modified:   index.html

--- assets/script.js
+++ assets/script.js
@@ -96,3 +96,23 @@ var swiper = new Swiper(".product-slider", {
       },
     },
 });
+
+var swiper = new Swiper(".review-slider", {
+    loop:true,
+    spaceBetween: 20,
+    autoplay:{
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1020: {
+        slidesPerView: 3,
+      },
+    },
+});
